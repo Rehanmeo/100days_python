e3ee7b5e3de63762241c573357794581fd14b95b
Muhammad Rehan <mrehanmeo@gmail.com>
2024-01-01 22:33:01 +0500

    Days 1 Python

---
+++ startpython/day1/first.py
@@ -0,0 +1,135 @@
+#first Work:
+# print("hello world")
+
+# creating variables
+# x = 3
+# y = "Name"
+
+# print(x)
+# print(y)
+
+# x = 3
+# x = "hello"
+# print(x)
+
+#casting
+# x = 8
+# print(x)
+# y = 3
+# z = float(x) / y
+# print(z)
+
+#getting of Type...
+# x = 3
+# y = "Rehan"
+# print(type(x))
+# print(type(y))
+
+# Many Value to Multiple Variables in one line
+
+# x, y, z = "Muhammad", "Rehan", "meo"
+# print(x)
+# print(y)
+# print(z)
+# print(x, y, z)
+
+#One Value to multiple Variables in one line
+
+# x = y = z = "Software engineer"
+# print(x)
+# print(y)
+# print(z)
+# print(x, y, z)
+
+#unpack a collection if you have a collectuib of values in a list, tuple etc, python allows you ti extract the value into variables. this is called unpacked
+# fruits = ["apple", "banana", "cherry"]
+# x, y, z = fruits
+# print(x, y, z)
+# a = fruits[0]
+# b = fruits[1]
+# c = fruits[2]
+# print(a)
+# print(b)
+# print(c)
+
+
+
+#Global Variables
+# x = "Muhammad Rehan"
+# def myfunc():
+#     print("python is  " + x)
+
+# myfunc()
+# print(x)
+
+# def myfunc1():
+#     global x
+#     x = "fantastic"
+
+# myfunc1()
+# print("yes python is " + x)
+
+#Python Data Type.....!
+# x =  str("Hello Worlds")
+# print(x)
+# print(type(x))
+
+# y = int(2)
+# print(y)
+# print(type(y))
+
+# z = float(20.5)
+# print(z)
+# print(type(z))
+
+# w = complex(1j)
+# print(w)
+# print(type(w))
+
+# v = list(("apple", "banana", "cherry"))
+# print(v)
+# print(type(v))
+
+# u = tuple(("apple", "banana", "cherry"))
+# print(u)
+# print(type(u))
+
+# t = range(6)
+# print(t)
+# print(type(t))
+
+# s = dict(name="John", age=36)
+# print(s)
+# print(type(s))
+
+# r = set(("apple", "banana", "cherry"))
+# print(r)
+# print(type(r))
+
+# q = frozenset(("apple", "banana", "cherry"))
+# print(q)
+# print(type(q))
+
+# p = bool(5)
+# print(p)
+# print(type(p))
+
+# o = bytes(5)
+# print(o)
+# print(type(o))
+
+# n = bytearray(5)
+# print(n)
+# print(type(n))
+
+# m = memoryview(bytes(5))
+# print(m)
+# print(type(m))
+
+#Random Numbers....!
+
+# import random
+
+# print(random.randrange(1, 200))
+
+
